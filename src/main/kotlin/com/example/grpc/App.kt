f4d358105a5ba462b589dadb4f2f166b1175738f
Dong-Hun Gang <1031nice@gmail.com>
2021-06-25 12:00:16 +0900

    과제 요구사항 전부를 담은 SimpleService.proto 작성(RegionsService.proto 제거)

--- src/main/kotlin/com/example/grpc/App.kt
+++ src/main/kotlin/com/example/grpc/App.kt
@@ -3,6 +3,11 @@ package com.example.grpc
 import com.linecorp.armeria.server.Server
 import com.linecorp.armeria.server.grpc.GrpcService
 
+/*
+    protocol buffer message의 필드 타입은 몽땅 string으로 하면 어떤 문제가 있을까
+    repeated field는 어떻게 주입하는걸까
+ */
+
 fun main(args: Array<String>) {
     val server = Server.builder()
         .http(8080)
