937a95bf7847071aff5cc96b7439de355c55e245
Dong-Hun Gang <1031nice@gmail.com>
2021-06-25 14:07:27 +0900

    Http API 핸들러 작성(URL만 매핑, 요청 매개변수 받는 것과 실제 핸들링 로직 미구현)

---
+++ src/main/kotlin/com/example/grpc/ArmeriaHttpClientDemo.kt
@@ -0,0 +1,38 @@
+package com.example.grpc
+
+import com.linecorp.armeria.server.Server
+import com.linecorp.armeria.server.annotation.Get
+import com.linecorp.armeria.server.annotation.Param
+import com.linecorp.armeria.server.annotation.Post
+
+class ArmeriaHttpClientDemo {
+    // 클라이언트는 nested인 경우 어떻게 던지지?
+    // 백에서는 그걸 binding해서 하나로 받을 수 있나?
+    @Post("/regions")
+    suspend fun test1(@Param("name") name: String,
+                      @Param("x") x: String,
+                      @Param("y") y: String): String {
+        return "name: $name, x: $x, y: $y"
+    }
+
+    @Post("/aois")
+    suspend fun test2(@Param("name") name: String,
+                      @Param("area") area: String): String {
+        return "name: $name, area: $area"
+    }
+
+    @Get("/regions/{regionId}/aois/intersects")
+    suspend fun test3(@Param("regionId") regionId: String): String {
+        return regionId
+    }
+}
+
+fun main() {
+    val server = Server.builder()
+        .http(8080)
+        .annotatedService()
+        .build(ArmeriaHttpClientDemo())
+        .build()
+    server.start()
+    println("server started!")
+}
