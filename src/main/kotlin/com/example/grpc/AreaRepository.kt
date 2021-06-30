90756ab752b893b9a28edf8078eac7f72ba6ffcb
Dong-Hun Gang <1031nice@gmail.com>
2021-06-30 17:15:28 +0900

    Kotest 의존성 추가, AreaRepositoryTest 생성

--- src/main/kotlin/com/example/grpc/AreaRepository.kt
+++ src/main/kotlin/com/example/grpc/AreaRepository.kt
@@ -9,8 +9,8 @@ import java.io.Serializable
 
 class AreaRepository<E: Area> {
 
-    val registry = StandardServiceRegistryBuilder().configure().build()
-    val sessionFactory = MetadataSources(registry).buildMetadata().buildSessionFactory()
+    private val registry = StandardServiceRegistryBuilder().configure().build()
+    private val sessionFactory = MetadataSources(registry).buildMetadata().buildSessionFactory()
 
     fun save(e: E): Serializable {
         try {
@@ -24,6 +24,17 @@ class AreaRepository<E: Area> {
         // TODO 예외처리
         catch(e: Exception) { println(e.message) }
         return -1
+    }
+
+    // TODO 제네릭으로 분명 될텐데
+    fun findAoi(id: Serializable): Any? {
+        val session = sessionFactory.openSession()
+        return session.find(Aoi::class.java, id)
+    }
+
+    fun findRegion(id: Serializable): Any? {
+        val session = sessionFactory.openSession()
+        return session.find(Region::class.java, id)
     }
 
     fun getAoisIntersectWithRegion(regionId: Int): List<Aoi> {
---
+++ src/test/kotlin/com/example/grpc/AreaRepositoryTest.kt
@@ -0,0 +1,44 @@
+package com.example.grpc
+
+import com.example.grpc.entity.Aoi
+import com.example.grpc.entity.Area
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.equality.shouldBeEqualToComparingFields
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.GeometryFactory
+
+internal class AreaRepositoryTest : FunSpec() {
+
+    init {
+
+        test("save") {
+            // When Aoi가 DB에 저장이 되면
+            val areaRepository = AreaRepository<Area>()
+            val aoi = Aoi()
+            aoi.name = "한라산"
+                val coordinates = arrayOf(
+                Coordinate(37.517386, 127.112990),
+                Coordinate(37.511687, 127.128048),
+                Coordinate(37.521286, 127.133858),
+                Coordinate(37.527938, 127.119110),
+                Coordinate(37.517386, 127.112990)
+            )
+            val factory = GeometryFactory()
+            var polygon = factory.createPolygon(coordinates)
+            aoi.area = polygon
+            val aoiId = areaRepository.save(aoi)
+
+            // Then 같은 id로 DB에서 불러온 것과 동등해야 한다
+            val find = areaRepository.findAoi(aoiId)
+            aoi shouldBeEqualToComparingFields find as Any
+        }
+
+        test("get AOIs by region id(intersects)") {
+//            val areaRepository = AreaRepository<Area>()
+//            val aois = areaRepository.getAoisIntersectWithRegion(1)
+            println("아직 작성되지 않은 테스트")
+        }
+
+    }
+
+}
